2284261cfa24a3467258a9e3b3224b75914e288e
GharrowBM <gharrow.games@gmail.com>
2022-04-01 10:10:02 +0200

    Refactoring

--- 01_Basics/sets.py
+++
@@ -1,9 +0,0 @@
-# Les Sets
-my_set = set()
-my_set.add("Bernie")
-my_set.add(3.5)
-my_set.add(666)
-print(f"my_set : {my_set}")
-strange_list = [1,1,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,3]
-strange_set = set(strange_list)
-print(f"strange_set : {strange_set}")
